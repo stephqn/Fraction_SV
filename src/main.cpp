53246484ed1f90ac948c5b2deeb5429e7838c03b
stephqn <shadowmen2@hotmail.com>
2014-05-16 15:44:29 +0200

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : main.cpp
+// Author      : Steeeeeeephan
+// Version     :
+// Copyright   : Your copyright notice
+// Description : DS Final
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	return 0;
+}
