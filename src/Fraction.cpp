b7fd768d19fc64a52cd734354c2053536fd02eab
stephqn <shadowmen2@hotmail.com>
2014-05-16 15:47:56 +0200

    Ajout de la calsse Fraction

---
+++ src/Fraction.cpp
@@ -0,0 +1,18 @@
+/*
+ * Fraction.cpp
+ *
+ *  Created on: 16 mai 2014
+ *      Author: stephan
+ */
+
+#include "Fraction.hpp"
+
+Fraction::~Fraction() {
+	// TODO Auto-generated destructor stub
+}
+
+Fraction::Fraction() {
+	// TODO Auto-generated constructor stub
+
+}
+
---
+++ src/Fraction.hpp
@@ -0,0 +1,17 @@
+/*
+ * Fraction.hpp
+ *
+ *  Created on: 16 mai 2014
+ *      Author: stephan
+ */
+
+#ifndef FRACTION_HPP_
+#define FRACTION_HPP_
+
+class Fraction {
+public:
+	virtual ~Fraction();
+	Fraction();
+};
+
+#endif /* FRACTION_HPP_ */
